17ac71c3c27c803fa9833653d5fc2f2d7ca7a258
Wutthiphat2402 <92678328+Wutthiphat2402@users.noreply.github.com>
2021-10-21 20:16:23 +0700

    Create code.py
    }:‑):-P:-!

---
+++ code.py
@@ -0,0 +1,57 @@
+import requests,json,time,threading,os,sys
+
+import urllib.request, os, threading, time, random, sys
+
+import colorama
+
+from colorama import Fore, Style, init
+
+#from requests_futures.sessions import FuturesSession
+
+#from requests_futures import sessions
+
+os.system("clear")
+
+os.system("figlet wow")
+
+print(Fore.RED + "[ภาระกิจส่งพิซซ่าให้ Buffalo🐃]")
+
+print("  [1] กด1เพื่อส่งพิซซ่า ")
+
+print("")
+
+verfly = input("number : ")
+
+if verfly == '1':
+
+  os.system("clear")
+
+time.sleep(1)
+
+os.system("clear")
+
+os.system("figlet sms")
+
+xxx = input ("ใส่เบอร์ Buffalo เพื่อเรียกเก็บเงิน💸 :")
+
+hee = input ("จำนวน ที่ต้องการส่งพิซซ่า:")
+
+time.sleep(2)
+
+print ("กำลังส่งพิซซ่า ให้🐃.....")
+
+time.sleep(0.9)
+
+os.system("clear")
+
+print ("ส่งพิซซ่าอยู่อย่ารีบ.....")
+
+time.sleep(3)
+
+print ("ส่งพิซซ่าสำเร็จ  ตังมา")
+
+time.sleep(0.01)
+
+print ("ส่งพิซซ่าสำเร็จ 💁 -.-")
+
+time.sleep(0.01)
